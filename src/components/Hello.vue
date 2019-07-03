216ea106b29ea644065c4bdc7df89364fe3043fe
原国超 <yuanguochao@yuanguochao.mzftech.cn>
2019-07-03 19:08:09 +0800

    update

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,13 +16,7 @@ export default Vue.extend({
         }
     },
     mounted(){
-      for (var i = 0; i < 10; i++) {
-        (function(i) {
-        setTimeout(function() { console.log(i); }, 100 * i);
-        })(i);
-      }
-      console.log(123);
-      
+        
     },
     methods: {
         increment() { this.enthusiasm++; },
